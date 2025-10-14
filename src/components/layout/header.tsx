0e48f06acce7b0926582642db3e0139638516271
Ekky Pradipta <eqpradipta@gmail.com>
2025-10-14 06:55:39 +0000

    hide dulu kolom pencarian (_for element input_)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -18,14 +18,16 @@ export function Header() {
     <header className="sticky top-0 z-10 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
       <SidebarTrigger className="sm:hidden" />
       
-      <div className="relative ml-auto flex-1 md:grow-0">
+      {/* <div className="relative ml-auto flex-1 md:grow-0">
         <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
         <Input
           type="search"
           placeholder="Cari..."
           className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[320px]"
         />
-      </div>
+      </div> */}
+
+      <div className="flex-1" />
 
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
