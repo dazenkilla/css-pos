20c12df9c579b2f4786843416dd4d343446aee37
Ekky Pradipta <eqpradipta@gmail.com>
2025-11-10 08:12:42 +0000

    Bro, bisa gak saat buka page itu langsung ke menu penjualan ?

--- next.config.ts
+++ next.config.ts
@@ -36,6 +36,15 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/sales',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
